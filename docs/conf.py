151fb9a8dc4b283cab6382c8a7e0b03b68a5681e
JeongSooNa <JeongSooNa@github.com>
2025-05-26 15:31:29 +0900

    new docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,3 +13,12 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+extensions = [
+    'myst_parser',
+]
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
