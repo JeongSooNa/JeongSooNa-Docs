28b85f2448fa932fc71a008523510106c5e934ec
JeongSooNa <JeongSooNa@github.com>
2025-05-26 15:09:23 +0900

    new docs

---
+++ docs/conf.py
@@ -0,0 +1,12 @@
+# docs/conf.py
+
+project = 'JeongSooNa Docs'
+author = 'JeongSooNa'
+release = '0.1'
+
+extensions = ['sphinx.ext.autodoc']
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
